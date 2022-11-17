062ee64a7465986bd6844f74909806e01b3095e2
Taej K <taest12@gmail.com>
2022-11-17 00:03:02 +0000

    .

--- market/macro/jolt.py
+++ market/macro/jolt.py
@@ -8,29 +8,29 @@ class Jolt:
     @staticmethod
     def jobOpenings_Nonfarm(mode='binary'):
         return (PlotEconomicIdx('JTSJOL').renameColumn('job Openings: Total Nonfarm')
-                .plot(title='job Openings: 비농업', mode=mode, y1_title=''))
+                .plot(title='job Openings: 비농업', mode=mode))
     @staticmethod    
     def jobOpenings_Private(mode='binary'):
         return (PlotEconomicIdx('JTS1000JOL').renameColumn('Job Openings: Total Private')
-                .plot(title='Job Openings: 민간', mode=mode, y1_title=''))
+                .plot(title='Job Openings: 민간', mode=mode))
         
     @staticmethod    
     def jobOpenings_Government(mode='binary'):
         return (PlotEconomicIdx('JTS9000JOL').renameColumn('Job Openings: Government')
-                .plot(title='Job Openings: 정부', mode=mode, y1_title=''))
+                .plot(title='Job Openings: 정부', mode=mode))
   
     @staticmethod    
     def hires_Nonfarm(mode='binary'):
         return (PlotEconomicIdx('JTSHIL').renameColumn('비농업 신규 채용수')
-                .plot(title='비농업 신규 채용수', mode=mode, y1_title=''))
+                .plot(title='비농업 신규 채용수', mode=mode))
     @staticmethod    
     def hires_Private(mode='binary'):
         return (PlotEconomicIdx('JTS1000HIL').renameColumn('민간 신규 채용수')
-                .plot(title='민간 신규 채용수', mode=mode, y1_title=''))
+                .plot(title='민간 신규 채용수', mode=mode))
     @staticmethod    
     def hires_Government(mode='binary'):
         return (PlotEconomicIdx('JTS9000HIL').renameColumn('정부 신규 채용수')
-                .plot(title='정부 신규 채용수', mode=mode, y1_title=''))
+                .plot(title='정부 신규 채용수', mode=mode))
     
     @staticmethod    
     def separations_Nonfarm(mode='binary'):
@@ -40,26 +40,26 @@ class Jolt:
     @staticmethod    
     def separations_Private(mode='binary'):
         return (PlotEconomicIdx('JTS1000TSL').renameColumn('이직건수 - 민간')
-                .plot(title='민간 이직건수', mode=mode, y1_title=''))
+                .plot(title='민간 이직건수', mode=mode))
     
     @staticmethod    
     def separations_Government(mode='binary'):
         return (PlotEconomicIdx('JTS9000TSL').renameColumn('이직건수 - 정부')
-                .plot(title='정부 이직건수', mode=mode, y1_title=''))
+                .plot(title='정부 이직건수', mode=mode))
     
     @staticmethod    
     def quits_Nonfarm(mode='binary'):
         return (PlotEconomicIdx('JTSQUL').renameColumn('자발적 퇴사 규모 - 비농업')
-                .plot(title='자발적 퇴사 규모 - 비농업', mode=mode, y1_title=''))
+                .plot(title='자발적 퇴사 규모 - 비농업', mode=mode))
     
     @staticmethod    
     def quits_Private(mode='binary'):
         return (PlotEconomicIdx('JTS1000QUL').renameColumn('자발적 퇴사 규모 - 민간')
-                .plot(title='자발적 퇴사 규모 - 민간', mode=mode, y1_title=''))
+                .plot(title='자발적 퇴사 규모 - 민간', mode=mode))
     
     @staticmethod    
     def quits_Government(mode='binary'):
         return (PlotEconomicIdx('JTS9000QUR').renameColumn('자발적 퇴사 규모 - 정부')
-                .plot(title='자발적 퇴사 규모 - 정부', mode=mode, y1_title=''))
+                .plot(title='자발적 퇴사 규모 - 정부', mode=mode))
     
      
