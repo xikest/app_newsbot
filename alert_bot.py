ea3f7fced6b802852bde4d7b1605a81f7b4ebe59
xikest <xikest12@gmail.com>
2022-11-21 01:34:27 +0900

    Update alert_bot.py

--- alert_bot.py
+++ alert_bot.py
@@ -10,7 +10,7 @@ TOKEN =BotInfo.AlertBot.get_token()
 SrcNews.setChatId(BotInfo.AlertBot.get_chanel('teat_chat_id'))
 SrcNews.Mailbox.setChatId(BotInfo.AlertBot.get_chanel('teat_w_chat_id'))
 SrcNews.NBER.setChatId(BotInfo.AlertBot.get_chanel('nber_chat_id'))
-# SrcNews.Tweets.setChatId(BotInfo.AlertBot.get_chanel('twt_chat_id'))
+SrcNews.Tweets.setChatId(BotInfo.AlertBot.get_chanel('twt_chat_id'))
 
 if platform.system()=='Windows':
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
