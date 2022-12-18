eccf6696c00ae07a9af26128be509e2b190b19e1
xikest <xikest12@gmail.com>
2022-12-19 07:04:02 +0900

    Update contents.py

--- tools/telegram_bot/contents.py
+++ tools/telegram_bot/contents.py
@@ -117,7 +117,7 @@ class Contents(list):
     
     def paragraphTrimming(self, paragraph):
         regexes = [
-            # r'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)', #http 주소 제거
+            r'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)', #http 주소 제거
             # r'[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)', #http 아닌 웹주소 제거
             'oott',
             'OOTT'
