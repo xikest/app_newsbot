b9ef80fce7fa6929d9f60bc07b0159e86c2c07f0
xikest <xikest12@gmail.com>
2023-10-14 12:41:21 +0900

    Update src_mail.py

--- bot/src/src_mail.py
+++ bot/src/src_mail.py
@@ -1,4 +1,4 @@
-from typing import Optional, Generator
+from typing import Optional, Generator, AsyncGenerator
 import imapclient
 import email
 from email.header import decode_header
@@ -16,55 +16,55 @@ class SrcMail:
         self._mailings = mailings
         self._chat_id: Optional[str] = chat_id
 
-    async   def generator(self) -> Context:
-                try:
-                    for mailing in self._mailings:
-                        UIDs, raw_msg = self._get_UIDs_msg(self._usr, self._pid, mailing.box)
-                        for UID in UIDs[-20:]:  # 최근 20개만 읽음
-                            message = email.message_from_bytes(raw_msg[UID][b'BODY[]'])
-                            fr = decode_header(message.get('From'))
-                            if mailing.sender in str(fr):
-                                body = None
-                                for part in message.walk():
-                                    ctype = part.get_content_type()
-                                    cdispo = str(part.get('Content-Disposition'))
-                                    body = part.get_payload(decode=True)  # decode
-                                    if body is not None:
-                                        await self._run_generator(ctype, cdispo, body, mailing)
-                    print(f'mail_src_fin: {datetime.datetime.now()}\n')
-                except Exception as e:
-                    print(f'mail_src_err -> sleep: {datetime.datetime.now()}')
-                    print(f'err -> {e}')
-                    await asyncio.sleep(30 * 60)
-                    print(f'mail_src_err -> awake: {datetime.datetime.now()}\n')
-
-    async   def _run_generator(self, ctype, cdispo, body, mailing):
-                urlPattern = '(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%.]+'
-
-                body = body.decode('utf-8')
-                if ctype == 'text/plain' and 'attachment' not in cdispo:
-                        urls = re.findall(urlPattern, body)
-                        for url in urls:
-                            if not mailing.conditions or all(condition in url for condition in mailing.conditions):
-                                await self._sent_url(url)
-
-                elif ctype == 'text/html' and 'attachment' not in cdispo:
-                        # BeautifulSoup을 사용하여 HTML 본문을 파싱
-                        soup = bs4.BeautifulSoup(body, 'html.parser')
-                        links = soup.find_all('a')
-                        for link in links:
-                            if link.text.strip() == "Read More":
-                                url = link.get('href')
-                                await self._sent_url(url)
 
 
+    async def generator(self) -> AsyncGenerator[Context, None]:
+        try:
+            for mailing in self._mailings:
+                UIDs, raw_msg = self._get_UIDs_msg(self._usr, self._pid, mailing.box)
+                for UID in UIDs[-20:]:
+                    message = email.message_from_bytes(raw_msg[UID][b'BODY[]'])
+                    fr = decode_header(message.get('From'))
+                    if mailing.sender in str(fr):
+                        body = None
+                        for part in message.walk():
+                            ctype = part.get_content_type()
+                            cdispo = str(part.get('Content-Disposition'))
+                            body = part.get_payload(decode=True)
+                            if body is not None:
+                                async for context in self._run_generator(ctype, cdispo, body, mailing):
+                                    yield context
+            print(f'mail_src_fin: {datetime.datetime.now()}\n')
+        except Exception as e:
+            print(f'mail_src_err -> sleep: {datetime.datetime.now()}')
+            print(f'err -> {e}')
+            await asyncio.sleep(30 * 60)
+            print(f'mail_src_err -> awake: {datetime.datetime.now()}\n')
 
-    async   def _sent_url(self, url):
-                sh = ps.Shortener()
-                url = sh.post.short(url)
-                # print(f"링크 주소: {url}")
-                await asyncio.sleep(10)
-                yield Context(content=[url], botChatId=self._chat_id, dtype='msg')
+    def _run_generator(self, ctype, cdispo, body, mailing):
+            urlPattern = '(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%.]+'
+
+            body = body.decode('utf-8')
+            if ctype == 'text/plain' and 'attachment' not in cdispo:
+                    urls = re.findall(urlPattern, body)
+                    for url in urls:
+                        if not mailing.conditions or all(condition in url for condition in mailing.conditions):
+                            yield from self._sent_url(url)
+
+            elif ctype == 'text/html' and 'attachment' not in cdispo:
+                    # BeautifulSoup을 사용하여 HTML 본문을 파싱
+                    soup = bs4.BeautifulSoup(body, 'html.parser')
+                    links = soup.find_all('a')
+                    for link in links:
+                        if link.text.strip() == "Read More":
+                            url = link.get('href')
+                            yield from self._sent_url(url)
+
+    def _sent_url(self, url):
+            sh = ps.Shortener()
+            url = sh.post.short(url)
+            # print(f"링크 주소: {url}")
+            yield Context(content=[url], botChatId=self._chat_id, dtype='msg')
 
     def _get_UIDs_msg(self, usr: str, pid: str, box: str, imap: str = 'imap.naver.com'):
         imap_obj = imapclient.IMAPClient(imap, ssl=True)
