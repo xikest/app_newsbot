4b88c2d99b1c44b8c011a93756287f77ed1373eb
xikest <94699546+xikest@users.noreply.github.com>
2024-04-18 21:47:30 +0900

    Update sender.py

--- info/sender.py
+++ info/sender.py
@@ -1,6 +1,6 @@
 class Sender:
     def __init__(self):
-        self.profiles = {'token':'5753698180:AAHlOqMGw_Pec-lYW6riVWDsCNiDBaybJ8I'}
+        self.profiles = {'token':'5753698180:AAEgXUBlwPeDmRGRGGmt9NpcE3JqyLYrMbU'}
     def get_token(self)->str:
         return self.profiles.get('token')
 
