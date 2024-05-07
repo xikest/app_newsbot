03d0f2c47e9bf2b1841e3230254660ac1edadd81
xikest <xikest12@gmail.com>
2024-05-07 20:19:18 +0900

    .

--- bot/src/src_news.py
+++ bot/src/src_news.py
@@ -6,39 +6,41 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 from functools import wraps
 
+
 class SrcNews:
-    def __init__(self, newsStand: Generator, chat_id:str=None, verbose=False):
-        self._chat_id:Optional[str]=chat_id
-        self._newsStand:Generator =  newsStand
+    def __init__(self, newsStand: Generator, chat_id: str = None, verbose=False):
+        self._chat_id: Optional[str] = chat_id
+        self._newsStand: Generator = newsStand
         self.verbose = verbose
-            
-    async def generator(self)-> Context:
-                for news in self._newsStand:
-                    try: 
-                        print(f"Start getting the feed from the {news.name}'s: {datetime.datetime.now()}") 
-                        if news.src == 'web': 
-                            webGenerator = WebScraper().get_links_general(url=news.url, class_key=news.class_key,
-                                                                          condition=lambda href: href.startswith('http') or href.startswith('www'))
-                            for content in webGenerator:
-                                yield Context(label=f'{news.name}', contents=[content],  botChatId=self._chat_id, dtype='msg')
-                        print(f"Finished obtaining the feed from the {news.name}'s : {datetime.datetime.now()}")
-                    except Exception as e:
-                        time_sleep = datetime.datetime.now()
-                        if self.verbose:
-                            print(f'Error description -> {e}')
-                            print(f"Raised a feed error from the {news.name}'s @{time_sleep}")
-                        await asyncio.sleep(60 * 10)  #다음 반복까지 대기 시간
-                        time_awake = datetime.datetime.now()
-                        if self.verbose:
-                            print(f"Awakened a feed error from the {news.name}'s @{time_awake}")
-                            print(f'Total sleep time{time_awake - time_sleep}')
+
+    async def generator(self) -> Context:
+        for news in self._newsStand:
+            try:
+                print(f"Start getting the feed from the {news.name}'s: {datetime.datetime.now()}")
+                if news.src == 'web':
+                    webGenerator = WebScraper().get_links_general(url=news.url, class_key=news.class_key,
+                                                                  condition=lambda href: href.startswith(
+                                                                      'http') or href.startswith('www'))
+                    for content in webGenerator:
+                        yield Context(label=f'{news.name}', contents=[content], botChatId=self._chat_id, dtype='msg')
+                print(f"Finished obtaining the feed from the {news.name}'s : {datetime.datetime.now()}")
+            except Exception as e:
+                time_sleep = datetime.datetime.now()
+                if self.verbose:
+                    print(f'Error description -> {e}')
+                    print(f"Raised a feed error from the {news.name}'s @{time_sleep}")
+                await asyncio.sleep(60 * 10)  # 다음 반복까지 대기 시간
+                time_awake = datetime.datetime.now()
+                if self.verbose:
+                    print(f"Awakened a feed error from the {news.name}'s @{time_awake}")
+                    print(f'Total sleep time{time_awake - time_sleep}')
 
 
 class WebScraper:
     def __init__(self, base_url=None):
         self.base_url = base_url
 
-    def get_links_general(self, url, class_key:str=None, prefix=None, condition=lambda href: True):
+    def get_links_general(self, url, class_key: str = None, prefix=None, condition=lambda href: True):
         full_url = self.base_url + url if self.base_url else url
         soup = BeautifulSoup(urlopen(full_url), 'html.parser')
         elements = soup.find_all(attrs={'class': f'{class_key}'})
@@ -47,6 +49,7 @@ class WebScraper:
                 result = self.link_filter(link, prefix, condition)
                 if result:
                     yield result
+
     def link_filter(self, link, prefix=None, condition=lambda x: True):
         href = link.attrs.get('href', '')
         if condition(href):
--- bot/src/src_rss.py
+++ bot/src/src_rss.py
@@ -7,41 +7,41 @@ from bs4 import BeautifulSoup
 
 from bot.handler.contents_hanlder import Context
 
+
 class SrcRss:
 
-    def __init__(self, rssList:Generator, chat_id:Optional[str], verbose = True):
-        self._chat_id:Optional[str]=chat_id
-        self._rssList:Generator = rssList
-        self.verbose =  verbose
+    def __init__(self, rssList: Generator, chat_id: Optional[str], verbose=True):
+        self._chat_id: Optional[str] = chat_id
+        self._rssList: Generator = rssList
+        self.verbose = verbose
 
-    async def generator(self)-> Context:
-                url = None
-                for rss in self._rssList:
-                    try:
-                        print(f"Start getting the feed from the {rss.name}'s: {datetime.datetime.now()}") 
-                        for feed in feedparser.parse(rss.url).entries[-10:]:
-                            if rss.src == 'googleAlert':
-                                
-                                url = feed.link.replace('https://www.google.com/url?rct=j&sa=t&url=','').split('&ct=ga&cd')[0]
-                                title = self.get_title(url)
-                                print(f"google rss title: {title}")
-                            elif rss.src == 'rss':
-                                url = feed.link
-                            if not rss.exceptions or all(exception not in url for exception in rss.exceptions):
-                                yield Context(label = f"{rss.name}", contents=[url], botChatId=self._chat_id, dtype='msg')
-                        print(f"Finished obtaining the feed from the {rss.name}'s : {datetime.datetime.now()}")     
-                        
-                    except Exception as e:
-                        time_sleep = datetime.datetime.now()
-                        if self.verbose:
-                            print(f'Error description -> {e}')
-                            print(f"Raised a feed error from the {rss.name}'s @{time_sleep}")
-                        await asyncio.sleep(60 * 10)  #다음 반복까지 대기 시간
-                        time_awake = datetime.datetime.now()
-                        if self.verbose:
-                            print(f"Awakened a feed error from the {rss.name}'s @{time_awake}")
-                            print(f'Total sleep time{time_awake - time_sleep}')
+    async def generator(self) -> Context:
+        url = None
+        for rss in self._rssList:
+            try:
+                print(f"Start getting the feed from the {rss.name}'s: {datetime.datetime.now()}")
+                for feed in feedparser.parse(rss.url).entries[-10:]:
+                    if rss.src == 'googleAlert':
 
+                        url = feed.link.replace('https://www.google.com/url?rct=j&sa=t&url=', '').split('&ct=ga&cd')[0]
+                        title = self.get_title(url)
+                        print(f"google rss title: {title}")
+                    elif rss.src == 'rss':
+                        url = feed.link
+                    if not rss.exceptions or all(exception not in url for exception in rss.exceptions):
+                        yield Context(label=f"{rss.name}", contents=[url], botChatId=self._chat_id, dtype='msg')
+                print(f"Finished obtaining the feed from the {rss.name}'s : {datetime.datetime.now()}")
+
+            except Exception as e:
+                time_sleep = datetime.datetime.now()
+                if self.verbose:
+                    print(f'Error description -> {e}')
+                    print(f"Raised a feed error from the {rss.name}'s @{time_sleep}")
+                await asyncio.sleep(60 * 10)  # 다음 반복까지 대기 시간
+                time_awake = datetime.datetime.now()
+                if self.verbose:
+                    print(f"Awakened a feed error from the {rss.name}'s @{time_awake}")
+                    print(f'Total sleep time{time_awake - time_sleep}')
 
     def get_title(self, url):
         try:
@@ -52,7 +52,7 @@ class SrcRss:
             if title_tag:
                 print(title_tag)
                 return title_tag.text.strip().lower()
-            
+
             else:
                 return "Title not found on the webpage."
 
