c8ed28962b58b31eed8b7b0a3efc3f4f36bcb060
xik <xikest12@gmail..com>
2024-10-24 21:14:46 +0900

    .

--- bot/bot_alert.py
+++ bot/bot_alert.py
@@ -6,8 +6,6 @@ from bot.src_alert import Src_Alert
 from info.sender import Sender
 
 class Bot_Alert():
-  STATUS = True
-  STOP = False
   def __init__(self):
     self._TOKEN = Sender().get_token()
     self._GPT = Sender().get_gptkey()
@@ -22,14 +20,13 @@ class Bot_Alert():
     return self._GPT
 
   async def start(self, waitTime:int = 1800):
-      while Bot_Alert.STATUS != Bot_Alert.STOP:
-                try:
-                    await self.update(self.src.generator)
-                    print(f'cycle finish, sleep {time.time()}')
-                    await asyncio.sleep(waitTime) #5분 대기
-                    print(f'awake{time.time()}')
-                except Exception as e:
-                    print(f'bot start err.{e}')
+    try:
+        await self.update(self.src.generator)
+        print(f'cycle finish, sleep {time.time()}')
+        await asyncio.sleep(waitTime) #5분 대기
+        print(f'awake{time.time()}')
+    except Exception as e:
+        print(f'bot start err.{e}')
 
   async def update(self, generatorForContext:Generator):
             start = time.time()
