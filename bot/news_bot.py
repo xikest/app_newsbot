db7701423ad25a568cdc877b6ced79daf04787aa
xik <xikest12@gmail..com>
2025-01-29 21:59:20 +0900

    .

--- bot/news_bot.py
+++ bot/news_bot.py
@@ -9,7 +9,7 @@ class NewsBot():
   def __init__(self):
     
     self.feeder = Feeder(feeds_path = 'https://raw.githubusercontent.com/xikest/app_newsbot/main/json/feeds.json')
-    self.feeder = Feeder(feeds_path = 'json/feeds.json')
+    # self.feeder = Feeder(feeds_path = 'json/feeds.json')
     
     self.firestore = "json/web-driver.json"
     self.bot_token = os.environ.get("BOT_TOKEN")
